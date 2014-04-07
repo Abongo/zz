7f60d13c74e02957affed26fa6eee42a72c26ae4
Percila Njira <njirap@gmail.com>
2014-04-07 12:20:37 +0300

    admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Admin.create(username: 'admin', password: 'admin')
+Admin.create! :password => ENV['ADMIN_PASSWORD'], :username => ENV['ADMIN_USERNAME']
