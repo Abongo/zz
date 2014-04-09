e4c56051077c604fa9a2ad920072a951e9e953df
Percila Njira <njirap@gmail.com>
2014-04-09 13:36:39 +0300

    UnInit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Admin.create! :password => 'admin', :username => 'admin'
-Forum.create! :name => 'Love', :description => 'Lets talk about all matters all kinds of Love here.'
-Forum.create! :name => 'Knowledge', :description => 'From Science to Arts to Religion, what do you know?'
-Forum.create! :name => 'True Leadership', :description => 'Whether leading a country or just leading yourself, lets hang out here.'
+
