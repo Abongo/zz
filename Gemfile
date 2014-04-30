27d8cca1438e79bc2556897d0cc87d080eeb0a07
jared abongo <abongoje@gmail.com>
2014-04-30 09:16:42 +0300

    styling styling

--- Gemfile
+++ Gemfile
@@ -15,10 +15,10 @@ end
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
-
+gem 'paperclip', '~> 3.0'
+gem 'omniauth-google-oauth2', '~> 0.2.4'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
-gem 'zurb-foundation'
 
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ActiveRecord::Base
 	def self.authenticate(username, password)
 		user = find_by_username_and_password(username, password)
 	end
+	def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    # unless user
+    #     user = User.create(name: data["name"],
+    #        email: data["email"],
+    #        password: Devise.friendly_token[0,20]
+    #     )
+    # end
+    user
+end
 end
